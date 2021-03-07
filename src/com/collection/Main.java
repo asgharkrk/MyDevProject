a3565966c889903702acbc7e5c89cc647bdec15f
Asghar Nazir <asghar@syntaxtech.com>
2021-03-07 11:22:54 -0500

    Testing pipeline

--- src/com/collection/Main.java
+++ src/com/collection/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 	// write your code here
-        for (int i = 0; i <6 ; i++) {
-            ystem.out.println("Hello batch 8");
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("Hello batch 8");
             Thread.sleep(2000);
         }
 
